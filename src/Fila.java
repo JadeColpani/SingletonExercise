035cb1840cab54eadfee6ca133f3b13706b6f47c
JadeColpani <jade.colpani@fatec.sp.gov.br>
2023-04-04 21:27:05 -0300

    SingletonExercise

---
+++ src/Fila.java
@@ -0,0 +1,22 @@
+public class Fila {
+
+    private static Fila instancia;
+    public static Fila getInstance() {
+        if (instancia == null) {
+            instancia = new Fila();
+        }
+        return instancia;
+    }
+    private Fila() {
+    }
+    public void ImprimeDocumento() {
+    }
+
+    public void RemoveDocumento() {
+    }
+
+    public void RemoveTodosDocumentos() {
+    }
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       Fila fila = Fila.getInstance();
+
+       Fila ImprimeDocumento = Fila.getInstance();
+       Fila RemoveDocumento = Fila.getInstance();
+       Fila RemoveTodosDocumentos = Fila.getInstance();
+
+        }
+    }
